8710cc97a1c0dfcb5e7a402e7e14f4daf0b81a77
Ananya Agrawal <109452550+ananyasgit@users.noreply.github.com>
2023-06-22 12:30:54 +0530

    Update head.py

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -38,37 +38,41 @@ class Detect(nn.Module):
         self.cv2 = nn.ModuleList(
             nn.Sequential(Conv(x, c2, 3), Conv(c2, c2, 3), nn.Conv2d(c2, 4 * self.reg_max, 1)) for x in ch)
         self.cv3 = nn.ModuleList(nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)
+        self.cv4 = nn.ModuleList(
+            nn.Sequential(Conv(x, c3, 3), Conv(c3, c3, 3), nn.Conv2d(c3, self.nc, 1)) for x in ch)  # New 4th channel conv
         self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
 
     def forward(self, x):
-        """Concatenates and returns predicted bounding boxes and class probabilities."""
-        shape = x[0].shape  # BCHW
+        z = []  # inference output
         for i in range(self.nl):
-            x[i] = torch.cat((self.cv2[i](x[i]), self.cv3[i](x[i])), 1)
+            if not self.training:  # if not training use cached shape
+                if self.dynamic:
+                    x[i] = F.interpolate(x[i], size=self.shape[i], mode='nearest')
+            x[i] = self.cv2[i](x[i])  # output conv
+            z.append(x[i].clone())  # inference output
+            if self.export:
+                continue  # export mode only outputs 2nd stage
+            x[i] = self.cv3[i](x[i])  # output conv
+            z.append(x[i].clone())  # inference output
+            x[i] = self.cv4[i](x[i])  # output conv for 4th channel
+            z.append(x[i].clone())  # inference output
         if self.training:
             return x
-        elif self.dynamic or self.shape != shape:
-            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
-            self.shape = shape
-
-        x_cat = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2)
-        if self.export and self.format in ('saved_model', 'pb', 'tflite', 'edgetpu', 'tfjs'):  # avoid TF FlexSplitV ops
-            box = x_cat[:, :self.reg_max * 4]
-            cls = x_cat[:, self.reg_max * 4:]
+        elif self.export:
+            return torch.cat(z, 1)
         else:
-            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
-        dbox = dist2bbox(self.dfl(box), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
-        y = torch.cat((dbox, cls.sigmoid()), 1)
-        return y if self.export else (y, x)
+            return x
+
 
     def bias_init(self):
-        """Initialize Detect() biases, WARNING: requires stride availability."""
-        m = self  # self.model[-1]  # Detect() module
-        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
-        # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
-        for a, b, s in zip(m.cv2, m.cv3, m.stride):  # from
-            a[-1].bias.data[:] = 1.0  # box
-            b[-1].bias.data[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
+    """Initialize Detect() biases, WARNING: requires stride availability."""
+    m = self  # self.model[-1]  # Detect() module
+    # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=nc) + 1
+    # ncf = math.log(0.6 / (m.nc - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # nominal class frequency
+    for a, b, c, s in zip(m.cv2, m.cv3, m.cv4, m.stride):  # from
+        a[-1].bias.data[:] = 1.0  # box
+        b[-1].bias.data[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (.01 objects, 80 classes, 640 img)
+        c[-1].bias.data[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls for 4th channel
 
 
 class Segment(Detect):
